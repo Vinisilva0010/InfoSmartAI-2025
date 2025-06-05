c026552eb1aadaddf3c60169f395591e95c35704
InfoSmart Demo <infosmart@example.com>
2025-06-04 21:38:33 -0300

    Corrigido case do import do CSS em Automacoes.tsx

--- src/pages/Automacoes.tsx
+++ src/pages/Automacoes.tsx
@@ -1,7 +1,7 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 import 'swiper/swiper-bundle.css';
-import '../styles/Automacoes.css';
+import '../styles/automacoes.css';
 import { Link } from 'react-router-dom';
 
 const automacoes = [
